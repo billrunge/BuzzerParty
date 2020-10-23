d14cb1b8e6e76da730a1ea9a162f83c34ec72eab
billrunge <billrunge@gmail.com>
2020-10-23 18:44:16 +0200

    Added GET api to delete all tables to prevent SQL from getting too large. Probably will run as a CRON job on Docker container.

---
+++ BuzzerParty/Controllers/DatabaseCleanupController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using BuzzerPartyLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuzzerParty.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatabaseCleanupController : ControllerBase
+    {
+        // POST api/Buzz
+        [HttpGet]
+        public async Task Get()
+        {
+            SqlConnectionString sqlConnectionString = new SqlConnectionString();
+            Database database = new Database() 
+            { 
+                SqlConnectionString = sqlConnectionString.GetSqlConnectionString() 
+            };
+
+            await database.DropAllTablesAsync();
+        }
+    }
+}
